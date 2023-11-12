46350dc4fb72a27e118e9c7622a1e6cf936afb44
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-12 21:49:36 +0000

    Mudanças

--- bank/src/App.jsx
+++ bank/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div classname={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+
+      </div>
+      <div className={`bg-primay ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+    </div>
   )
 }
 
