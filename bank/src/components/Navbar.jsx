0a499845d0818e6c91bd88f2fcf7a4ae6b95c0f7
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-14 15:56:25 +0000

    criação navbar e inicio parte main

--- bank/src/components/Navbar.jsx
+++ bank/src/components/Navbar.jsx
@@ -29,6 +29,26 @@ const Navbar = () => {
           className='w-[28px] h-[28px] object-contain'
           onClick={()=> setToggle((prev)=> !prev)}
         />
+        <div className={`${toggle?'flex':'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.lenght - 1 ? 'mr-0' : 'mb-4'} text-white`}
+              >
+
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+
+              </li>
+
+            ))}
+          </ul>
+
+
+        </div>
       </div>
     </nav>
   )
