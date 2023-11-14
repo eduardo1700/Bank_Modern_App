e486ac038d73f42814506857387e3f1fb6633b95
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-14 13:34:34 +0000

    criação navbar computador e inicio navbar telemovel

--- bank/src/components/Navbar.jsx
+++ bank/src/components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from "../assets"
+import { navLinks } from '../constants'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} className='w-[124px] h-[32px]' />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.lenght - 1 ? 'mr-0' : 'mr-10'} text-white`}
+          >
+
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+
+          </li>
+
+        ))}
+      </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center ">
+        <img
+          src={toggle ? close : menu}
+          className='w-[28px] h-[28px] object-contain'
+          onClick={()=> setToggle((prev)=> !prev)}
+        />
+      </div>
+    </nav>
   )
 }
 
