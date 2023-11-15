fd0deecfcbe99139daffbbeb8a0fea616c2a4288
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-15 17:00:30 +0000

    continuação do trabalho de hoje

--- bank/src/components/Hero.jsx
+++ bank/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { discount, robot } from '../assets'
+import GetStarted from './GetStarted'
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -8,12 +9,36 @@ const Hero = () => {
         <div className='flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'>
           <img src={discount} className='w-[32px] h-[32px]' />
           <p className={`${styles.paragraph} ml-2`}>
-          <span className='text-white'>20%</span> Discount for {" "}
-          <span className='text-white'>1 Month</span> Account
+            <span className='text-white'>20%</span> Discount for {" "}
+            <span className='text-white'>1 Month</span> Account
           </p>
         </div>
-      </div>
 
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
+            The Next <br className='sm:block hidden' />{" "}
+            <span className='text-gradient'>Generation
+            </span>
+          </h1>
+
+          <div className='ss:flex hidden md:mr-4 mr-0'>
+            <GetStarted />
+          </div>
+        </div>
+        <h1 className='font-poppins font-semibold ss:text-[68 px] text-[52px] text-white ss:leading-[100px] leading-[75px]'> Payment Method.</h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates, annual fees.</p>
+
+      </div>
+      <div>
+        <img src={robot} className='w-[100%] h-[100%] relative z-[5]' />
+
+        <div className='absolute z-[0] w-[20%] h-[35%] top-20 pink__gradient rounded-full blu' />
+
+        <div className='absolute z-[1] w-[70%] h-[80%] bottom-40 top-20 rounded-full white__gradient' />
+
+        <div className='absolute z-[0] w-[50%] h-[50%] bottom-20 right-20 rounded-full blue__gradient' />
+        
+      </div>
     </section>
   )
 }
