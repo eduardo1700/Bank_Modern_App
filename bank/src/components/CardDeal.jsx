660e2d1446c6036c512b4b864bc5997782347a89
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-20 17:58:50 +0000

    Finalizar projeto banco

--- bank/src/components/CardDeal.jsx
+++ bank/src/components/CardDeal.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Discover a world of financial possibilities with our streamlined process. We make finding the perfect card deal simple and efficient.
+      </p>
 
-export default CardDeal
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
