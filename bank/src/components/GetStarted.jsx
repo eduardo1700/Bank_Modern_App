59a843331ce3356d8c86ba8777cc66d7df005c82
eduardo1700 <edu.alex.mp@gmail.com>
2023-11-20 11:39:58 +0000

    adicionar hover

--- bank/src/components/GetStarted.jsx
+++ bank/src/components/GetStarted.jsx
@@ -3,8 +3,8 @@ import styles from '../style'
 import { arrowUp } from '../assets'
 const GetStarted = () => {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
-      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer  `}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full hover:opacity-30  `}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
             <span className='text-gradient'>
